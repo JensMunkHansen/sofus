3d7bc4f89267c2e344868772f688f91b2d240d9b
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2016-12-03 11:27:43 +0100

    ..

--- cmake/DoxygenTarget.cmake
+++ cmake/DoxygenTarget.cmake
@@ -6,19 +6,19 @@ macro (ADD_DOXYGEN_TARGET Target)
   file(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/.tmp")
 
   add_custom_command(
-    OUTPUT ${PROJECT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/html/index.html
-    COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/Doxyfile
+    OUTPUT ${PROJECT_SOURCE_DIR}/${CMAKE_DOXYGEN_OUT}/html/index.html
+    COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
     DEPENDS
     COMMENT "Generating API documentation with Doxygen" VERBATIM)
 
-  add_custom_target( doc DEPENDS ${PROJECT_BINARY_DIR}/doc/html/index.html )
+  add_custom_target( doc DEPENDS ${PROJECT_SOURCE_DIR}/doc/html/index.html )
 
   if(WIN32)
     install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/html
       DESTINATION Documentation/${Target}
       COMPONENT Documentation)
   else()
-    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/html
+    install(DIRECTORY ${PROJECT_SOURCE_DIR}/doc/html
       DESTINATION "${INSTALL_DOC_DIR}/${Target}" COMPONENT dev)
   endif()
   SET(CPACK_NSIS_MENU_LINKS
