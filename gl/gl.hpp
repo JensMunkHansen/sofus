9032c22f31a48d2d08c78bf57630391bcd7104ca
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2017-06-10 13:08:25 +0200

    copyrights

--- gl/gl.hpp
+++ gl/gl.hpp
@@ -14,6 +14,23 @@
 #include <gl/config.h>
 #include <stddef.h>
 
+/*
+ *  This file is part of SOFUS.
+ *
+ *  SOFUS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SOFUS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SOFUS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /** @defgroup GL Gauss-Legendre library
  *  @brief A library for computing nodes and weights for quadrature using Gauss-Legendre polynomials.
  *
