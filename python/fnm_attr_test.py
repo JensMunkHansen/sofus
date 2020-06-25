02d78f7d25699dccf89f7f14cb8924cc3c6ad2f1
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2020-06-26 00:48:25 +0200

    test not parsing, but everything compiles

--- python/fnm_attr_test.py
+++ python/fnm_attr_test.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8; tab-width: 2; python-indent: 2; indent-tabs-mode: nil -*-
 
 import sys
 import os
 import re
 import numpy as np
+
+from sys import version_info
 
 import unittest
 import subprocess
@@ -18,28 +21,31 @@ class FnmTest(unittest.TestCase):
   def setUp(self):
     """
     For some reason, the unittest framework returns an error
-    
+
     SyntaxError: unqualified exec is not allowed in function
     'test_properties_set' because it contains a nested function
     with free variables
-    
+
     Workaround is to introduce the member nSetMethods
     """
     setmethods = fnm.ApertureFloat.__swig_setmethods__.keys()
-    self.nSetMethods = len(filter(lambda i: not re.compile('^_').search(i), setmethods))
+    self.nSetMethods = len(tuple(filter(lambda i: not re.compile('^_').search(i), setmethods)))
   def test_properties_get(self):
 
-    # Filter out builtin_function_type
-    getmethods = {k: v for k,v in fnm.ApertureFloat.__swig_getmethods__.iteritems() if type(v) != types.BuiltinFunctionType}
-    # Filter out lambda functions (constructors)
-    getmethods = {k: v for k,v in getmethods.iteritems() if v.func_name != '<lambda>'}.keys()
+    if version_info < (3, 0, 0):
+      # Filter out builtin_function_type
+      getmethods = {k: v for k,v in fnm.ApertureFloat.__swig_getmethods__.iteritems() if type(v) != types.BuiltinFunctionType}
+      # Filter out lambda functions (constructors)
+      getmethods = {k: v for k,v in getmethods.iteritems() if v.func_name != '<lambda>'}.keys()
 
-    # Update without pulsed waves also
-    if 'FNM_PULSED_WAVE' in fnm.__dict__.keys():
-      self.assertEqual(len(getmethods), 32)
+      # Update without pulsed waves also
+      if 'FNM_PULSED_WAVE' in fnm.__dict__.keys():
+        self.assertEqual(len(getmethods), 34)
+      else:
+        self.assertEqual(len(getmethods), 23)
     else:
-      self.assertEqual(len(getmethods), 23)
-      
+      getmethods = {k: v for k,v in fnm.ApertureFloat.__swig_getmethods__.items()}
+
     argss = [[0,0.1,0.1,0.1],[5,0.1,0.1,0.1],[100,0.1,0.1,0.1]]
     nGetSuccess = 0
     testMe = set()
@@ -54,21 +60,26 @@ class FnmTest(unittest.TestCase):
             raise(Exception('error getting '+method))
     except Exception as e:
       print(e.message)
-  
+
     self.assertEqual(nGetSuccess/3,len(getmethods))
   def test_properties_set(self):
     setmethods = fnm.ApertureFloat.__swig_setmethods__.keys()
 
+    nSetMethods = 0
     if 'FNM_PULSED_WAVE' in fnm.__dict__.keys():
-      self.assertEqual(len(setmethods),26)
+      nSetMethods=28
     else:
-      assert(len(setmethods)==17)
+      nSetMethods=17
+
+    #if 'FNM_CLOSURE_FUNCTIONS' in fnm.__dict__.keys():
+    #  nSetMethods = nSetMethods + 3
+
+    self.assertEqual(len(setmethods),nSetMethods)
 
     argss = [[0,0.1,0.1,0.1],[1,0.1,0.1,0.1],[100,0.1,0.1,0.1]]
 
     nSetSuccess = 0
     testMe = set()
-    method = setmethods[0]
     try:
       for args in argss:
         a = fnm.ApertureFloat(*args)
@@ -87,10 +98,10 @@ class FnmTest(unittest.TestCase):
     self.assertEqual(nSetSuccess/3,self.nSetMethods)
 
   def test_closures_dimensions(self):
-    if fnm.ApertureFloat.__dict__.keys().count('RwFloatParamSetMulti') > 0:
+    if tuple(fnm.ApertureFloat.__dict__.keys()).count('RwFloatParamSetMulti') > 0:
 
       a = fnm.ApertureFloat(5,1.0,0.0,1.0)
-      
+
       if 'FNM_PULSED_WAVE' in fnm.__dict__.keys():
         params = [fnm.RwParamType.Excitation,
                   fnm.RwParamType.Impulse]
@@ -101,7 +112,7 @@ class FnmTest(unittest.TestCase):
           a.RwFloatParamSetMulti(param, refValue, 1, 10)
           testValue = a.RwFloatParamGet(param,1)[1]
           self.assertTrue(np.all(refValue == testValue))
-        
+
       params = [fnm.RwParamType.ElementDelays,
                 fnm.RwParamType.Apodization,
                 fnm.RwParamType.Focus,
@@ -114,10 +125,10 @@ class FnmTest(unittest.TestCase):
         a.RwFloatParamSetMulti(param, refValue, 1, len(refValue))
         testValue = a.RwFloatParamGet(param,1)[1]
         self.assertTrue(np.all(refValue == testValue))
-      
+
   def test_closures_scalar_float(self):
     # Not compiling using MSVC 2013
-    if fnm.ApertureFloat.__dict__.keys().count('RwFloatParamSetMulti') > 0:
+    if tuple(fnm.ApertureFloat.__dict__.keys()).count('RwFloatParamSetMulti') > 0:
       # TODO: Expose list of properties from C/C++
       props = [[fnm.RwParamType.Alpha, 'alpha'],
                [fnm.RwParamType.Beta,  'beta'],
@@ -135,7 +146,7 @@ class FnmTest(unittest.TestCase):
         self.assertEqual(value,refValue)
         value1 = a.RwFloatParamGet(prop[0],0)[1]
         self.assertEqual(value1,refValue)
-    
+
   def test_setting_elements(self):
     a = fnm.ApertureFloat()
     elements0 = np.random.rand(32,8).astype(np.float32)
@@ -143,7 +154,7 @@ class FnmTest(unittest.TestCase):
     elements1 = a.elements
     self.assertTrue(np.all(elements0==elements1))
 
-    if fnm.ApertureFloat.__dict__.keys().count('RwFloatParamSetMulti') > 0:
+    if tuple(fnm.ApertureFloat.__dict__.keys()).count('RwFloatParamSetMulti') > 0:
       elements2 = np.random.rand(32,8).astype(np.float32)
       a.RwFloatParamSetMulti(fnm.RwParamType.Elements, elements2, 2, 32, 8)
       elements3 = a.elements
@@ -155,14 +166,12 @@ class FnmTest(unittest.TestCase):
     a.subelements = subelements0
     subelements1 = a.subelements
     self.assertTrue(np.all(subelements0==subelements1))
-        
+
 if __name__ == '__main__':
   unittest.main()
 
 # Local variables: #
-# indent-tab-mode: nil #
 # tab-width: 2 #
 # python-indent: 2 #
-# py-indent-offset: 2 #
 # indent-tabs-mode: nil #
 # End: #
