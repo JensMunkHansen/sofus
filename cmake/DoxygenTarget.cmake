755d4a3aa77e5545fcad6ea40f938a2ec2b32959
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2020-06-26 00:26:50 +0200

    doxygen trouble

--- cmake/DoxygenTarget.cmake
+++ cmake/DoxygenTarget.cmake
@@ -1,17 +1,23 @@
 macro (ADD_DOXYGEN_TARGET Target)
-  set(CMAKE_DOXYGEN_OUT doc)
+  # Figure this out
+  set(CMAKE_DOXYGEN_OUT "${Target}")
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY IMMEDIATE)
 
-  # New way
+  # Figure this out to specify doxy file
   file(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/.tmp")
 
+  # Consider replacing DOXYGEN_EXECUTABLE with unbuffer sh -c "doxygen configfile 2>/proc/$$/fd/2" | animation 
+  
   add_custom_command(
-    OUTPUT ${PROJECT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/html/index.html
-    COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/Doxyfile
-    DEPENDS
+    OUTPUT ${PROJECT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/doxygen.stamp  # Was ${PROJECT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/html/index.html
+    COMMAND ${DOXYGEN_EXECUTABLE} -b ${PROJECT_BINARY_DIR}/Doxyfile
+    COMMAND ${CMAKE_COMMAND} -E touch ${PROJECT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/doxygen.stamp
+    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile # Was empty
     COMMENT "Generating API documentation with Doxygen" VERBATIM)
 
-  add_custom_target( doc DEPENDS ${PROJECT_BINARY_DIR}/doc/html/index.html )
+  # DEPENDS ${PROJECT_BINARY_DIR}/doc/html/index.html
+  # ALL is removed to prevent it from building all the time
+  add_custom_target( "${Target}" DEPENDS ${PROJECT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/doxygen.stamp)
 
   if(WIN32)
     install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_DOXYGEN_OUT}/html
