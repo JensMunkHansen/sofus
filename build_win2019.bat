718d89a0e258c91a627a2c1ecdcc5f5a1e5d45a2
Jens Munk Hansen <jem@bkmedical.com>
2022-05-09 14:02:03 +0200

    VS2017 almost there - FFTW is missing

--- build_win2019.bat
+++ build_win2019.bat
@@ -4,18 +4,12 @@ REM This can be made a lot more elegant - focus is right to make it work
 
 pushd %~dp0
 
-set CONDAPATH=C:\ProgramData\Anaconda2\envs\Python37
 
-REM set QTDIR=C:\Qt\Qt5.12.9\5.12.9\msvc2017_64
-REM set PYPIPATH=c:/Users/jem/Environments/fis
-REM set PYPIPATH=c:/ProgramData/Anaconda3
-REM REM set PYPIPATH=c:/Users/jem/AppData/Local/Programs/Python/Python36
-REM set PYVER=37
 set SEDPATH=c:/cygwin64/bin
-REM set GRAPHVIZ_PATH=C:\Program Files (x86)\Graphviz\bin
-REM set SWIG_DIR=C:\Program Files\SWIG4.0
+set GRAPHVIZ_PATH=C:\Program Files (x86)\Graphviz2.38\bin
+set SWIG_DIR=C:\Program Files\SWIG
 REM REM set SWIG_DIR=C:\Program Files\swig
-REM set DOXYGEN_DIR=C:\Program Files\doxygen\bin
+set DOXYGEN_DIR=C:/PFx64/doxygen
 
 set(GTEST_ROOT="C:/Program Files/googletest-distribution")
 
@@ -23,14 +17,15 @@ set FFTWDIR="C:\Program Files\fftw-3.3.5"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+call "c:\cygwin64\home\jem\Environments\PyPI38\Scripts\activate.bat"
 
 mkdir build
 
-set PATH="C:\Program Files\CMake\bin";%SWIG_DIR%;%CONDAPATH%;%SEDPATH%;%PATH%
+set PATH="C:\Program Files\CMake\bin";%SWIG_DIR%;%SEDPATH%;%GRAPHVIZ_PATH%;%PATH%
 
 REM ;%DOXYGEN_DIR%;%GRAPHVIZ_PATH%;%SWIG_DIR%;%PATH%;%SEDPATH%
 
-cmake -H%~dp0 -B%~dp0\build -G "Visual Studio 16 2019" -A "x64" -DBUILD_SPS_TEST=ON -DBUILD_DOCUMENTATION=OFF -DBUILD_FNM_TEST=ON -DBUILD_GL_TEST=ON -DPYTHON_INCLUDE_DIR=%CONDAPATH%\include -DPYTHON_LIBRARY=%CONDAPATH%\libs\python37.lib -DPYTHON_EXECUTABLE=%CONDAPATH%\python.exe
+cmake -H%~dp0 -B%~dp0\build -G "Visual Studio 16 2019" -A "x64" -DBUILD_SPS_TEST=OFF -DBUILD_DOCUMENTATION=ON -DBUILD_SWIG_DOCUMENTATION=ON -DBUILD_FNM_TEST=ON -DBUILD_GL_TEST=ON -DDOXYGEN_EXECUTABLE=%DOXYGEN_DIR%/bin/doxygen.exe -DPYTHON_EXECUTABLE="C:\cygwin64\home\jem\Environments\PyPI38\Scripts\python.exe"
 
 REM Create solution
 cd %~dp0\build
